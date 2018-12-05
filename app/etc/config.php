6cdf9e1fc4e1bc82622b44da393455dbeb375909
Preeti Ambekar <preetijav@gmail.com>
2018-12-05 11:43:16 +0100

    Cookies Set

--- app/etc/config.php
+++ app/etc/config.php
@@ -13,6 +13,7 @@ return [
         'Learning_GreetingMessage' => 1,
         'Learning_HelloPage' => 1,
         'Learning_Js' => 1,
+        'Magecomp_Cookiecompliance' => 1,
         'Magento_Catalog' => 1,
         'Magento_AdvancedPricingImportExport' => 1,
         'Magento_Config' => 1,
